0c033b98b54aab78008768a2f7a38b4858165696
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2020-09-09 21:14:06 +0530

    Updates

---
+++ precache-manifest.794abd5fe1aec1f17e907eed6f6e2120.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bed48a21029cc04739d3b4f5871d68",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "3e654469b4e2dc6dc9a9",
+    "url": "/react-weather-app/static/css/main.58490b5a.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/react-weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e654469b4e2dc6dc9a9",
+    "url": "/react-weather-app/static/js/main.d231cc89.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  },
+  {
+    "revision": "dfaf92a4580a18b213fd0142fe6f6697",
+    "url": "/react-weather-app/static/media/cold.dfaf92a4.jpg"
+  },
+  {
+    "revision": "0495cd845eff39ac80ac2b4cca7692c5",
+    "url": "/react-weather-app/static/media/hot.0495cd84.jpg"
+  }
+]);
